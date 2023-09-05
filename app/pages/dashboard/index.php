6d0413b012d68584c7db13d36caa58e7ab0ec448
jijihadi <50286299+jijihadi@users.noreply.github.com>
2023-09-05 21:49:48 +0700

    Fix: Dashboard

--- app/pages/dashboard/index.php
+++ app/pages/dashboard/index.php
@@ -14,22 +14,19 @@ isLogin();
         <!-- partial -->
         <div class="main-panel">
             <div class="content-wrapper">
-                <div class="page-header">
-                    <h3 class="page-title">
-                        <span class="page-title-icon bg-gradient-primary text-white me-2">
-                            <i class="mdi mdi-home"></i>
-                        </span> Dashboard
-                    </h3>
-                    <nav aria-label="breadcrumb">
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item active" aria-current="page">
-                                <span></span>Overview <i
-                                    class="mdi mdi-alert-circle-outline icon-sm text-primary align-middle"></i>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
                 <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <img src="<?= "$baseurl/assets/images/bg.jpg"?>" alt="" style="width:100%;">
+                                <h4 class="card-title my-2">RSPAD Gatot Soebroto</h4>
+                                <p>Rumah Sakit Pusat Angkatan Darat (RSPAD) Gatot Soebroto yang megah dan besar dan merupakan Rumah Sakit RUJUKAN tertinggi bagi Rumah Sakit TNI di seluruh penjuru Nusantara, dibangun oleh Pemerintah Belanda pada bulan Oktober 1936 dengan nama “Groot Militair Hospitaal Weltevreden”.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- <div class="row">
                     <div class="col-md-4 stretch-card grid-margin">
                         <div class="card bg-gradient-danger card-img-holder text-white">
                             <div class="card-body">
@@ -93,7 +90,7 @@ isLogin();
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
             <!-- content-wrapper ends -->
             <!-- partial:partials/_footer.html -->
